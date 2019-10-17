e763c7171e91f2bcc780251bf45057cec6273c09
rilika <53854591+rilika@users.noreply.github.com>
2019-10-17 21:18:18 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,14 +1,11 @@
-def func(arr,k):
-	if k in arr:
-		return True
-	else:
-		return False
+def presence(a):
+    li=[1,4,5,2,6]
+    if a in li:
+      print("element present")
+    else:
+      print("not present")
+    
 
-n=int(input("enter number of elements"))
-arr=[]
-for i in range(0,n):
-	ele=int(input())
-	arr.append(ele)
-k=int(input("enter search element"))
-print(func(arr,k))
+a=int(input("enter a number"))
+presence(a)
 
